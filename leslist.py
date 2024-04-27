102d2204fb7d26280acc5b804eda067b21196678
robin18j <123131793+robin18j@users.noreply.github.com>
2024-04-27 12:29:11 -0400

    udate

--- leslist.py
+++ leslist.py
@@ -1,16 +1,29 @@
 import nid_poule 
 import datetime
+import LatLon
 #liste 
 global prioritaire
 global Nprioritaire
 global y # facteur qui determine si prioritaire ou non
+global datacomplete
+# ajouter a liste et blocker si deja existant
 def ajouter_a_liste(x): # est une objet nidpoule
     if (x.profondeur==y) :
-        if not (prioritaire.index(x.latitude) and prioritaire.index(x.longitude)):
+        if not (prioritaire.index(x.coordinates)and erreurT (x,prioritaire) ):
             prioritaire.append(x)
     else :
-        if not (Nprioritaire.index(x.latitude) and Nprioritaire.index(x.longitude)):
+        if not (Nprioritaire.index(x.coordinates)and erreurT (x,Nprioritaire)):
             Nprioritaire.append(x)
+# retourn faux si a l<interieur de 5 m pour reduire 
+def erreurT (x,y):
+    T=x.coordinates
+    for i in y:
+        z=i.coordinates
+        d=T.distantce.z
+        if not  d> 0.005 :
+         return False
+    return True
+    
 
 #envoyer list de travail au employer
 def get_day_off_work():
@@ -30,7 +43,7 @@ def gdownp(dlistofwork):
         return dlistofwork
     
     return gdownp(dlistofwork.append(Nprioritaire.pop(0)))
-
+#remettre dans les listes les element non-reparer dans la liste de tache a faire
 def noncomplete (dlistofwork):
     for x in dlistofwork:
          ajouter_a_liste(x)
@@ -38,6 +51,10 @@ def noncomplete (dlistofwork):
     prioritaire.sort(key=lambda date: datetime.strptime(date, "%D"))
     Nprioritaire.sort(key=lambda date: datetime.strptime(date, "%D"))
 
-
-
-
+#suprimer les element reparer dans la liste de tache a faire
+def complete (dlistofwork):
+    for x in dlistofwork:
+        if (prioritaire.index(x.coordinates) and  False==erreurT (x,prioritaire)):
+           prioritaire.pop(prioritaire.index(x.coordinates))
+        if  (Nprioritaire.index(x.coordinates) and False==erreurT (x,Nprioritaire)):
+             Nprioritaire.pop(Nprioritaire.index(x.coordinates))
--- nidobj.py
+++ nidobj.py
@@ -1,10 +1,12 @@
 import datetime
 from PIL import Image
+import time
+import LatLon
+
 class nid_poule:
-    def __init__( latitude , longitude,profondeur,photo):# photo = Image.open(path)
-        latitude =latitude
-        longitude= longitude
-        profondeur=profondeur
+    def __init__( coordinates ,profondeur,LatLon,photo):# photo = Image.open(path)
+        coordinates =coordinates 
+        palmyra = LatLon(x, y) 
         date= datetime.datetime.now()
         photo= photo
-
+        time.sleep(1) 
