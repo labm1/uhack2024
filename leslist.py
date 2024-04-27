9987e995f2ec689b2530d6b70758ad2f011b8c98
robin18j <123131793+robin18j@users.noreply.github.com>
2024-04-27 09:52:07 -0400

    pytoncode
    code python objet nidp et fonction pour le serveur

---
+++ leslist.py
@@ -0,0 +1,10 @@
+import datetime
+from PIL import Image
+class nid_poule:
+    def __init__( latitude , longitude,profondeur,time,photo):# photo = Image.open(path)
+        latitude =0.0 
+        longitude= 0.0
+        profondeur=profondeur
+        time= datetime.datetime.now()
+        photo= photo
+
---
+++ nidobj.py
@@ -0,0 +1,9 @@
+import datetime
+from PIL import Image
+class nid_poule:
+    def __init__( latitude , longitude,time,photo):# photo = Image.open(path)
+        latitude =0.0 
+        longitude= 0.0
+        time= datetime.datetime.now()
+        photo= photo
+
