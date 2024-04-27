44dfdc8aa32258838d4982cbe1db86b006ba9060
robin18j <123131793+robin18j@users.noreply.github.com>
2024-04-27 09:55:58 -0400

    realpythoncode

--- leslist.py
+++ leslist.py
@@ -1,10 +1,25 @@
-import datetime
-from PIL import Image
-class nid_poule:
-    def __init__( latitude , longitude,profondeur,time,photo):# photo = Image.open(path)
-        latitude =0.0 
-        longitude= 0.0
-        profondeur=profondeur
-        time= datetime.datetime.now()
-        photo= photo
+import nid_poule 
+
+#liste 
+global prioritaire
+global Nprioritaire
+def ajouter_a_liste(x): # est une objet nidpoule
+    if (x.profondeur==y) :
+        if not (prioritaire.index(x.latitude) and prioritaire.index(x.longitude)):
+            prioritaire.append(x)
+    else :
+        if not (Nprioritaire.index(x.latitude) and Nprioritaire.index(x.longitude)):
+            Nprioritaire.append(x)
+
+#envoyer list de travail au employer
+def get_day_off_work():
+    return gdow(dlistofwork)
+
+def gdow(dlistofwork):
+    if (len(prioritaire)+len(Nprioritaire)==0 or (len(dlistofwork)==40)):
+        return dlistofwork
+    if not prioritaire.len(0):
+        return gdow(dlistofwork.append(prioritaire.pop(0)))
+    else:
+        return gdow(dlistofwork.append(Nprioritaire.pop(0)))
 
--- nidobj.py
+++ nidobj.py
@@ -1,9 +1,10 @@
 import datetime
 from PIL import Image
 class nid_poule:
-    def __init__( latitude , longitude,time,photo):# photo = Image.open(path)
-        latitude =0.0 
-        longitude= 0.0
+    def __init__( latitude , longitude,profondeur,time,photo):# photo = Image.open(path)
+        latitude =latitude
+        longitude= longitude
+        profondeur=profondeur
         time= datetime.datetime.now()
         photo= photo
 
