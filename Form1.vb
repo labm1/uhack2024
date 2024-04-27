6abc2baaee7c9c8e57c1df5bb48abe8b99f56f6f
labm1 <122836234+labm1@users.noreply.github.com>
2024-04-27 11:28:54 -0400

    test

--- Form1.vb
+++ Form1.vb
@@ -1,10 +1,4 @@
-﻿Imports GMap.NET
-Imports System.Windows.Forms.VisualStyles.VisualStyleElement
-Imports GMap.NET.MapProviders
-Imports GMap.NET.WindowsForms
-Imports GMap.NET.WindowsForms.Markers
-
-Public Class Form1
+﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Panel1.BackColor = Color.FromArgb(13, 72, 127)
         PictureBox1.BackColor = Color.FromArgb(13, 72, 127)
