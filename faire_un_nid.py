aacdda2f4626e9eab513ab3ba5c583c6a3fe69cd
robin18j <123131793+robin18j@users.noreply.github.com>
2024-04-27 18:11:49 -0400

    update

--- faire_un_nid.py
+++ faire_un_nid.py
@@ -6,6 +6,7 @@ from geopy.geocoders import Nominatim
 import nidobj
 import numpy as np 
 from PIL import Image as im 
+import requests
 #pour les request 311
 #x est un string entrer par les cityen ou employer 
 def request(x,photo):
@@ -18,6 +19,9 @@ def request(x,photo):
     n311= nidobj.nid_poule(profondeur,y,numpy_array)
     #envoyer vers serveur avec AI
     # vers_IA(n311)
+    url=HTTP//132.213.172.11:8080/
+    requests.post(url, n311)
+
 
 #pour notre systeme automatique
 #x serais la position donner par le gps
@@ -26,4 +30,7 @@ def req_automatique(x,photo):
     numpy_array = np.array(photo)
     auto= nidobj.nid_poule(profondeur,y,numpy_array)
      #envoyer vers serveur avec AI
+    url=HTTP//132.213.172.11:8080/
+    requests.post(url, auto)
     time.sleep(1) #temps pour eviter 
+    
---
+++ import time.py
@@ -0,0 +1,5 @@
+import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+hostName="Localhost"
+serverport =8080
